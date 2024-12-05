ec3213f9d275338a2899158d36cefe457ada49b9
Stanvic <stanvicbest@gmail.com>
2024-12-05 14:39:05 +0100

    deploy

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,13 @@
 <?php
 // DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_USER", "stanvicc_sharefaith");
+define("DB_PASS", "sharefaith2025");
 define("DB_NAME", "shareposts");
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/shareposts');
+define('URLROOT', 'https://social.stanvic.com.ng');
 // Site Name
 define('SITENAME', 'Sharefaith');
