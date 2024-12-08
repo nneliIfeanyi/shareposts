dbf88dfcc08324f609160070f765c79e81e54000
Stanvic <stanvicbest@gmail.com>
2024-12-08 09:56:27 +0100

    server error corrected

--- app/config/config.php
+++ app/config/config.php
@@ -1,28 +1,26 @@
 <?php
 // DB Params
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "shareposts");
-
-// App Root
-define('APPROOT', dirname(dirname(__FILE__)));
-// URL Root
-define('URLROOT', 'http://localhost/shareposts');
-// Site Name
-define('SITENAME', 'Sharefaith');
-
-
-// <?php
-// // DB Params
 // define("DB_HOST", "localhost");
-// define("DB_USER", "stanvicc_sharefaith");
-// define("DB_PASS", "sharefaith2025");
-// define("DB_NAME", "stanvicc_shareposts");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "shareposts");
 
 // // App Root
 // define('APPROOT', dirname(dirname(__FILE__)));
 // // URL Root
-// define('URLROOT', 'https://social.stanvic.com.ng');
+// define('URLROOT', 'http://localhost/shareposts');
 // // Site Name
 // define('SITENAME', 'Sharefaith');
+
+// DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "stanvicc_sharefaith");
+define("DB_PASS", "sharefaith2025");
+define("DB_NAME", "stanvicc_shareposts");
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'https://social.stanvic.com.ng');
+// Site Name
+define('SITENAME', 'Sharefaith');
