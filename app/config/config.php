3059c6fab9ab199e0e04ddd8efb87d010342283d
Stanvic <stanvicbest@gmail.com>
2024-12-08 10:01:27 +0100

    navbar-adjust

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,22 @@
 <?php
 // DB Params
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "shareposts");
+
+// // App Root
+// define('APPROOT', dirname(dirname(__FILE__)));
+// // URL Root
+// define('URLROOT', 'http://localhost/shareposts');
+// // Site Name
+// define('SITENAME', 'Sharefaith');
+
+// DB Params
 define("DB_HOST", "localhost");
 define("DB_USER", "stanvicc_sharefaith");
 define("DB_PASS", "sharefaith2025");
-define("DB_NAME", "shareposts");
+define("DB_NAME", "stanvicc_shareposts");
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md py-2 bg-dark-subtle mb-3">
+<nav class="navbar navbar-expand-md pb-2 fixed-top bg-dark-subtle mb-3">
   <div class="container">
     <a class="navbar-brand fw-lighter" href="<?php echo URLROOT; ?>/pages"><?php echo SITENAME; ?></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
