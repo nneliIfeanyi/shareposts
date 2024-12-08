2ad857baa5ff2a8b20a84055c4ab83ea6599e62e
Stanvic <stanvicbest@gmail.com>
2024-12-08 09:53:56 +0100

    navbar-adjust2

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,28 @@
 <?php
 // DB Params
 define("DB_HOST", "localhost");
-define("DB_USER", "stanvicc_sharefaith");
-define("DB_PASS", "sharefaith2025");
+define("DB_USER", "root");
+define("DB_PASS", "");
 define("DB_NAME", "shareposts");
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'https://social.stanvic.com.ng');
+define('URLROOT', 'http://localhost/shareposts');
 // Site Name
 define('SITENAME', 'Sharefaith');
+
+
+// <?php
+// // DB Params
+// define("DB_HOST", "localhost");
+// define("DB_USER", "stanvicc_sharefaith");
+// define("DB_PASS", "sharefaith2025");
+// define("DB_NAME", "stanvicc_shareposts");
+
+// // App Root
+// define('APPROOT', dirname(dirname(__FILE__)));
+// // URL Root
+// define('URLROOT', 'https://social.stanvic.com.ng');
+// // Site Name
+// define('SITENAME', 'Sharefaith');
