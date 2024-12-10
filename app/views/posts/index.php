a8a50a8527ee8bd51d1642cd1e58e29663723258
Stanvic <stanvicbest@gmail.com>
2024-12-10 13:54:05 +0100

    postsView-retouch

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,15 +1,14 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-
 <div style="margin-top: 78px;"></div>
 <?php flash('post_message'); ?>
 <?php foreach ($data['posts'] as $post) : ?>
-  <div class="card card-body">
-    <h4 class="card-title"><?php echo $post->title; ?></h4>
+  <div class="card card-body mb-3">
+    <h4 class="card-title fst-italics fw-light"><?php echo $post->title; ?></h4>
     <div class="bg-light p-2 mb-3">
       <?php if (empty($post->edited)): ?>
-        Written by <?php echo $post->name; ?> | <span class="fw-lighter fst-italic"><?php echo $post->day; ?></span>
+        <?php echo $post->name; ?> | <span class="fw-lighter fst-italic"><?php echo $post->day; ?></span>
       <?php else: ?>
-        Written by <?php echo $post->name; ?> | last edited: <span class="fw-lighter fst-italic"><?php echo $post->edited; ?></span>
+        <?php echo $post->name; ?> | last edited: <span class="fw-lighter fst-italic"><?php echo $post->edited; ?></span>
       <?php endif; ?>
     </div>
     <p class="card-text"><?php echo $post->body; ?></p>
