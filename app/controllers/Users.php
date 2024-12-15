b50eb19e7ba3e50f2087089f1ee48e84eb9413e3
Stanvic <stanvicbest@gmail.com>
2024-12-15 14:23:50 +0100

    cookie adjust

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -5,7 +5,7 @@ class Users extends Controller
   public $postModel;
   public function __construct()
   {
-    if (isset($_COOKIE['user_id'])) {
+    if (!isset($_COOKIE['user_id'])) {
       redirect('users/login');
     } else {
       $_SESSION['user_id'] = $_COOKIE['user_id'];
