cce4f79701e51aad1fa11fa98e9ccbc34772dfca
Stanvic <stanvicbest@gmail.com>
2024-12-15 14:00:25 +0100

    cokkie sett

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -9,6 +9,7 @@ class Users extends Controller
       redirect('users/login');
     } else {
       $_SESSION['user_id'] = $_COOKIE['user_id'];
+      redirect('posts');
     }
     $this->userModel = $this->model('User');
     $this->postModel = $this->model('Post');
